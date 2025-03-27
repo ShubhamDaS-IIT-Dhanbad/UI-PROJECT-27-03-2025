1e9ee6969a3bd319686f9b1ca88002334de75983
BharatLinkerRetailer <bharatlinkerretailer@gmail.com>
2025-03-27 20:09:44 +0530

    push-01

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>{children} hihih</body>
+      <body>{children} h</body>
     </html>
   );
 }
